bd4591e5b9fbe82e55997aaafbc40cbf0e841733
Al3xa66o <alexadobbato@gmail.com>
2024-07-11 11:11:42 +0200

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -1,22 +1,67 @@
-let computerPoint = 0;
-let humanPoint = 0;
 
-function getComputerChoice () {
-    let numRandom = Math.floor(Math.random() * 3)
 
-    if (numRandom == 1) {
-        console.log("rock");
-    }
-    else if (numRandom == 2) {
-        console.log("scissor");
-    }
-    else if (numRandom == 3) {
-        console.log("paper");
+function getComputerChoice() {
+    const random = ["rock", "paper", "scissor"];
+    const randomChoice = Math.floor(Math.random() * random.length);
+    return random[randomChoice];
+}
+
+function getHumanChoice() {
+    var humanChoice = prompt("chose a sign. ").toLowerCase();
+    return humanChoice
+}
+
+
+let compScore = 0;
+let humScore = 0;
+
+const humanSel = getHumanChoice();
+const computerSel = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+    
+    if ((humanChoice == "rock") && (computerChoice == "rock")) {
+        console.log("its a tie")
+    } else if ((humanChoice == "paper") && (computerChoice == "paper")) {
+        console.log("its a tie")
+    } else if ((humanChoice == "scissor") && (computerChoice == "scissor")) {
+        console.log("its a tie")
+    } else if ((humanChoice == "rock") && (computerChoice == "scissor")) {
+        humScore ++;
+        console.log("player won, rock beat scissor")
+    } else if ((humanChoice == "scissor") && (computerChoice == "paper")) {
+        humScore ++;
+        rconsole.log("player won, scissor beat paper")
+    } else if ((humanChoice == "paper") && (computerChoice == "rock")) {
+        humScore ++;
+        console.log("player won, paper beat rock")
+    } else if ((humanChoice == "rock") && (computerChoice == "paper")) {
+        compScore ++;
+        console.log("computer won, paper beat rock")
+    } else if ((humanChoice == "paper") && (computerChoice == "scissor" )) {
+        compScore ++;
+        console.log("computer won, scissor beat paper")
+    } else if ((humanChoice == "scissor") && (computerChoice == "rock")) {
+        compScore ++;
+        console.log("computer won, rock beat scissor")
     }
 }
 
-function getHumanChoice () {
-    let choice = prompt("chose a sign.");
-
-    console.log("you chosen: " + choice);
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound(getComputerChoice(), getHumanChoice());
+    }
+    if (compScore > humScore) {
+        console.log (`you lose! your score: ${humScore}, comp score: ${compScore}`)
+    } else if (compScore < humScore) {
+        console.log (`you win! your score: ${humScore}, comp score: ${compScore}`)
+    }
 }
+
+playGame();
+ 
+
+
+
+
+
